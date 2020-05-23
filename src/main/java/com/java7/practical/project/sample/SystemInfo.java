2692f82785dc038ada7dff910f7819237543a0cb
LaurisVerins <Taurus200!>
2020-05-23 16:08:10 +0300

    first

---
+++ src/main/java/com/java7/practical/project/sample/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.java7.practical.project.sample;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
